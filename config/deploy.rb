95dc13b66effdbfe340e93d71386583f41f6a07d
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2012-05-29 18:23:22 -0500

    Fix capistrano config for new hosting

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,13 +4,15 @@ set :repository,  "git://github.com/zacharydanger/boxcarsurgery.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
+set :deploy_to, "/var/apps/#{application}"
+
 set :user, "deploy"
 
 set :keep_releases, 5
 
-role :web, "ns3.designsauce.org"                          # Your HTTP server, Apache/etc
-role :app, "ns3.designsauce.org"                          # This may be the same as your `Web` server
-role :db,  "ns3.designsauce.org", :primary => true # This is where Rails migrations will run
+role :web, "boxcarsurgery.com"                          # Your HTTP server, Apache/etc
+role :app, "boxcarsurgery.com"                          # This may be the same as your `Web` server
+role :db,  "boxcarsurgery.com", :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
